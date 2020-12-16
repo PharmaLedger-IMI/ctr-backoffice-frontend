0cc3ed9788ecb2f76834ae3e171237e3cecc908d
jpsl <joao.luis@pdmfc.com>
2020-12-16 14:55:30 +0000

    appresource list and editing

--- src/app/appresource/appresource.component.ts
+++ src/app/appresource/appresource.component.ts
@@ -9,19 +9,26 @@ import { APPRESOURCES } from '../mock-appresource';
 })
 export class AppresourceComponent implements OnInit {
 
-  arc: AppResource = {
+  /* arc: AppResource = {
       id: 1,
       key: 'x',
       locale: { code: "en" },
       value: 'value',
       help: 'help'
-  };
+  }; */ // not used anymore
 
-  arcCollection = APPRESOURCES;
+  arcCollection = APPRESOURCES; /* collection of all AppResources */
+
+  arcSelected?: AppResource; // needed ? to indicate optional !???
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(arc: AppResource): void {
+      this.arcSelected = arc;
+      console.log("Selected Arc.id=", this.arcSelected.id);
   }
 
 }
